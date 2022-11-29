58f42abbcbd30417fd84f2da1b6809878a45baf3
Igor <trishin.igor@icloud.com>
2022-11-29 22:30:55 +0300

    teast

---
+++ src/дз11_Task1/PassDate.java
@@ -0,0 +1,20 @@
+package дз11_Task1;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class PassDate
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Фамилия разработчика: ");
+        Scanner in = new Scanner(System.in);
+        String sname = in.next();
+        Date date = new Date();
+        System.out.println("Дата получения задания: Wed Nov 02 01:42:16 MSK 2022"
+                +"\n"
+                + "Фамилия разработчика: " + sname
+                + "\n"
+                + "Дата и время сдачи задания: " + date);
+    }
+}
